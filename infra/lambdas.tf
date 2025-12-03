d992e143d429fc0f515ad2ee90c739158a339506
Igor Nunes Patricio <igornunespatricio@gmail.com>
2025-12-02 21:51:24 -0300

    add lambda resource

--- infra/lambdas.tf
+++ infra/lambdas.tf
@@ -45,3 +45,27 @@ resource "aws_lambda_permission" "apigw_get" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
+
+resource "aws_lambda_function" "get_ranking_month" {
+  function_name = "getFutevoleiRankingMonth"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+
+  filename         = "${path.module}/../backend/get_ranking_month/get_ranking_month.zip"
+  source_code_hash = filebase64sha256("${path.module}/../backend/get_ranking_month/get_ranking_month.zip")
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.games.name
+    }
+  }
+}
+
+resource "aws_lambda_permission" "apigw_get_ranking" {
+  statement_id  = "AllowAPIGatewayInvokeRanking"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_ranking_month.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
