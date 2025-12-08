9f7972e8516e9db7511352a57b76c72b8afcce4c
Igor Nunes Patricio <igornunespatricio@gmail.com>
2025-12-08 20:28:16 -0300

    change provider to s3 and dynamo db

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 1.5"
+
+  backend "s3" {
+    bucket         = "asc-ftv-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "asc-ftv-terraform-locks"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
