5347d99a1c9132fff4bcd6b86f5ba84cd2069b0d
Igor Nunes Patricio <igornunespatricio@gmail.com>
2025-12-09 20:43:22 -0300

    add players resouurce to api gateway terraform

--- infra/apigateway.tf
+++ infra/apigateway.tf
@@ -182,6 +182,11 @@ resource "aws_api_gateway_deployment" "api_deployment" {
         aws_api_gateway_method.get_games.http_method,
         aws_api_gateway_method.get_ranking.http_method,
         aws_api_gateway_method.options_ranking.http_method,
+
+        aws_api_gateway_method.post_players.http_method,
+        aws_api_gateway_method.get_players.http_method,
+        aws_api_gateway_method.options_players.http_method,
+        aws_api_gateway_method.delete_player.http_method,
       ]
       integrations = [
         aws_api_gateway_integration.post_games_integration.id,
@@ -189,9 +194,15 @@ resource "aws_api_gateway_deployment" "api_deployment" {
         aws_api_gateway_integration.get_games_integration.id,
         aws_api_gateway_integration.get_ranking_integration.id,
         aws_api_gateway_integration.options_ranking_integration.id,
+
+        aws_api_gateway_integration.post_players_integration.id,
+        aws_api_gateway_integration.get_players_integration.id,
+        aws_api_gateway_integration.options_players_integration.id,
+        aws_api_gateway_integration.delete_player_integration.id,
       ]
     }))
   }
+
 
   lifecycle {
     create_before_destroy = true
@@ -258,4 +269,134 @@ resource "aws_cloudwatch_log_group" "api_gw_logs" {
       Name = "${terraform.workspace}-apigw-logs"
     }
   )
+}
+
+# ---------------------------------------------------------
+# PLAYERS RESOURCE
+# ---------------------------------------------------------
+
+resource "aws_api_gateway_resource" "players_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "players"
+}
+
+# ---------------------------------------------------------
+# OPTIONS /players (CORS)
+# ---------------------------------------------------------
+
+resource "aws_api_gateway_method" "options_players" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.players_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options_players_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.players_resource.id
+  http_method = aws_api_gateway_method.options_players.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+
+  passthrough_behavior = "WHEN_NO_MATCH"
+}
+
+resource "aws_api_gateway_method_response" "options_players_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.players_resource.id
+  http_method = aws_api_gateway_method.options_players.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_players_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.players_resource.id
+  http_method = aws_api_gateway_method.options_players.http_method
+  status_code = aws_api_gateway_method_response.options_players_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "''"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,DELETE,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "''"
+  }
+
+  response_templates = {
+    "application/json" = ""
+  }
+}
+
+# ---------------------------------------------------------
+# POST /players (Create Player)
+# ---------------------------------------------------------
+
+resource "aws_api_gateway_method" "post_players" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.players_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "post_players_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.players_resource.id
+  http_method             = aws_api_gateway_method.post_players.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.create_player.invoke_arn
+}
+
+# ---------------------------------------------------------
+# GET /players (List Players)
+# ---------------------------------------------------------
+
+resource "aws_api_gateway_method" "get_players" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.players_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get_players_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.players_resource.id
+  http_method             = aws_api_gateway_method.get_players.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.list_players.invoke_arn
+}
+
+# ---------------------------------------------------------
+# /players/{id} for DELETE
+# ---------------------------------------------------------
+
+resource "aws_api_gateway_resource" "player_id_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_resource.players_resource.id
+  path_part   = "{id}"
+}
+
+resource "aws_api_gateway_method" "delete_player" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.player_id_resource.id
+  http_method   = "DELETE"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "delete_player_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.player_id_resource.id
+  http_method             = aws_api_gateway_method.delete_player.http_method
+  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  uri                     = aws_lambda_function.delete_player.invoke_arn
 }
