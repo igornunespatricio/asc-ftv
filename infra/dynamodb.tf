40c7dafe37e353533f4b890d139c48da21ee12fe
Igor Nunes Patricio <igornunespatricio@gmail.com>
2025-12-09 20:02:14 -0300

    add dynamo db table for players

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -33,3 +33,33 @@ resource "aws_dynamodb_table" "games" {
     }
   )
 }
+
+resource "aws_dynamodb_table" "players" {
+  name         = "asc-ftv-${terraform.workspace}-players"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+
+  # nickname é opcional e não precisa de index ou key, mas pode ser declarado
+  attribute {
+    name = "nickname"
+    type = "S"
+  }
+
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "asc-ftv-${terraform.workspace}-players"
+    }
+  )
+}
