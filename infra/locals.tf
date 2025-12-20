b9bfa08fa33e2e80e1a2666ac5a9315eee3ce101
Igor Nunes Patricio <igornunespatricio@gmail.com>
2025-12-20 18:55:09 -0300

    add source dir param in locals

--- infra/locals.tf
+++ infra/locals.tf
@@ -12,58 +12,50 @@ locals {
   lambda_configs = {
     add_game = {
       function_name = "asc-ftv-${terraform.workspace}-add-game"
-      env = {
-        GAMES_TABLE = module.games_table.table_name
-      }
+      source_dir    = "${path.root}/../lambdas/add_game"
+      env           = { GAMES_TABLE = module.games_table.table_name }
     }
 
     get_games = {
       function_name = "asc-ftv-${terraform.workspace}-get-games"
-      env = {
-        GAMES_TABLE = module.games_table.table_name
-      }
+      source_dir    = "${path.root}/../lambdas/get_games"
+      env           = { GAMES_TABLE = module.games_table.table_name }
     }
 
     create_player = {
       function_name = "asc-ftv-${terraform.workspace}-create-player"
-      env = {
-        PLAYERS_TABLE = module.players_table.table_name
-      }
+      source_dir    = "${path.root}/../lambdas/create_player"
+      env           = { PLAYERS_TABLE = module.players_table.table_name }
     }
 
     delete_player = {
       function_name = "asc-ftv-${terraform.workspace}-delete-player"
-      env = {
-        PLAYERS_TABLE = module.players_table.table_name
-      }
+      source_dir    = "${path.root}/../lambdas/delete_player"
+      env           = { PLAYERS_TABLE = module.players_table.table_name }
     }
 
     update_player = {
       function_name = "asc-ftv-${terraform.workspace}-update-player"
-      env = {
-        PLAYERS_TABLE = module.players_table.table_name
-      }
+      source_dir    = "${path.root}/../lambdas/update_player"
+      env           = { PLAYERS_TABLE = module.players_table.table_name }
     }
 
     list_players = {
       function_name = "asc-ftv-${terraform.workspace}-list-players"
-      env = {
-        PLAYERS_TABLE = module.players_table.table_name
-      }
+      source_dir    = "${path.root}/../lambdas/list_players"
+      env           = { PLAYERS_TABLE = module.players_table.table_name }
     }
 
     get_ranking_month = {
       function_name = "asc-ftv-${terraform.workspace}-get-ranking-month"
-      env = {
-        GAMES_TABLE = module.games_table.table_name
-      }
+      source_dir    = "${path.root}/../lambdas/get_ranking_month"
+      env           = { GAMES_TABLE = module.games_table.table_name }
     }
 
     jwt_authorizer = {
       function_name = "asc-ftv-${terraform.workspace}-jwt-authorizer"
-      env = {
-        JWT_SECRET = var.jwt_secret
-      }
+      source_dir    = "${path.root}/../lambdas/jwt_authorizer"
+      env           = { JWT_SECRET = var.jwt_secret }
     }
   }
 }
